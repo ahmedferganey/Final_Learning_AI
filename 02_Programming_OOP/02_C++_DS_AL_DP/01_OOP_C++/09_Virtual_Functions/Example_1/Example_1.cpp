d3016e71f2196419dd8ac946adc4fa18aeb5dc3f
ahmedferganey <ahmed.ferganey707@gmail.com>
2024-10-23 20:28:05 +0300

    OOP_C++

--- 02_Programming_OOP/02_C++_DS_AL_DP/01_OOP_C++/09_Virtual_Functions/Example_1/Example_1.cpp
+++ 02_Programming_OOP/02_C++_DS_AL_DP/01_OOP_C++/09_Virtual_Functions/Example_1/Example_1.cpp
@@ -1,6 +1,17 @@
 // Example_1.cpp - Example code for 09_Virtual_Functions
 #include <iostream>
 
+using namespace std;
+
+// base class
+class Animal{
+public:
+    // virtual function that can be overidden
+    virtual void sound(){
+        cout << "Animal makes a sound" << endl;
+    }
+}
+
 int main() {
     std::cout << "Example_1 example for 09_Virtual_Functions" << std::endl;
     return 0;
