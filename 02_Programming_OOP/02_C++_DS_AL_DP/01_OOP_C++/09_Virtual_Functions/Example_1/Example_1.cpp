32f0a7b5a58c2ee648159c84cfb20cd8bb383037
ahmedferganey <ahmed.ferganey707@gmail.com>
2024-10-23 21:06:44 +0300

    OOP_C++

--- 02_Programming_OOP/02_C++_DS_AL_DP/01_OOP_C++/09_Virtual_Functions/Example_1/Example_1.cpp
+++ 02_Programming_OOP/02_C++_DS_AL_DP/01_OOP_C++/09_Virtual_Functions/Example_1/Example_1.cpp
@@ -10,9 +10,54 @@ public:
     virtual void sound(){
         cout << "Animal makes a sound" << endl;
     }
-}
+    // virtual destructor ensures proper cleanup
+    virtual ~Animal(){
+        cout << "Animal destructor called waa waa waa" << endl;
+    }
+};
+
+// Derived class Dog overriding the base class sound() method
+class Dog : public Animal {
+
+public:
+    // override the virtual function
+    void sound() override{
+        cout << "Dog barks" << endl;
+    }
+    // Destructor
+    ~Dog(){
+        cout << "Dog destructor called" << endl;
+    }
+};
+
+// Derived class Cat overriding the base class's sound() method
+class Cat : public Animal {
+public:
+    // Override the virtual function
+    void sound() override {
+        cout << "Cat meows" << endl;
+    }
+
+    // Destructor
+    ~Cat() {
+        cout << "Cat destructor called" << endl;
+    }
+};
 
 int main() {
     std::cout << "Example_1 example for 09_Virtual_Functions" << std::endl;
+
+    // base class pointer to Dog object
+    Animal* animalptr = new Dog();
+    animalptr->sound();  // Calls Dog's overridden sound() method
+    delete animalptr;    // Calls Dog's destructor, then Animal's destructor
+
+    // Base class pointer to Cat object
+    animalptr = new Cat();
+    animalptr->sound();  // Calls Cat's overridden sound() method
+    delete animalptr;    // Calls Cat's destructor, then Animal's destructor
+
+
+
     return 0;
 }
