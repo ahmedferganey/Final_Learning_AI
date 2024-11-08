d45ef2f220548fad1b5509dc68873e24385c509b
ahmedferganey <ahmed.ferganey707@gmail.com>
2024-11-08 21:57:43 +0200

    Cleaning Repo

--- 02_Programming_OOP/02_C++_DS_AL_DP/01_OOP_C++/07_Operator_Overloading/Example_1/Example_1.cpp
+++
@@ -1,7 +0,0 @@
-// Example_1.cpp - Example code for 07_Operator_Overloading
-#include <iostream>
-
-int main() {
-    std::cout << "Example_1 example for 07_Operator_Overloading" << std::endl;
-    return 0;
-}
