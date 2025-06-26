94c2563525122ac991ed493aa0884cc55fc0a2e8
ahmedferganey <ahmed.ferganey707@gmail.com>
2025-06-26 15:15:08 +0300

    Updated

---
+++ 08_Embedded_AI_FrontEnd/FrontEnd/Practice/reactapp/src/components/Todo.jsx
@@ -0,0 +1,11 @@
+function Todo(){
+    return (
+        <div className="todo">
+            <h2>Todo Component</h2>
+            <p>This is a simple Todo component.</p>
+        </div>
+    );
+
+}
+
+export default Todo;
