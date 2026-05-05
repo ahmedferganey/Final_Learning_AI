b66be592c166c0d1180b251bf09f1d422114d695
ahmedferganey <ahmed.ferganey707@gmail.com>
2026-05-05 19:25:17 +0300

    uploading file

---
+++ 09_Gen_AI/02_rag/project/mini_rag_app/src/routes/data.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status, Request
+from fastapi.responses import JSONResponse
+import os
+from helpers.config import get_settings, Settings
+from controllers import DataController, ProjectController
+import aiofiles
+from models import ResponseSignal
+import logging
+
+
+logger = logging.getLogger('uvicorn.error')
+
+data_router = APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1", "data"],
+)
+
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(
+    request: Request,
+    project_id: int,
+    file: UploadFile,
+    app_settings: Settings = Depends(get_settings)
+):
+
+    data_controller = DataController()
+
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": result_signal
+            }
+        )
+
+    # ✅ FIX: unpack tuple
+    file_path, saved_file_name = data_controller.generate_unique_filepath(
+        orig_file_name=file.filename,
+        project_id=project_id
+    )
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+
+        logger.error(f"Error while uploading file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_name": saved_file_name
+        }
+    )
